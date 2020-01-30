02a856624f7d5206444c7a0294d06fd3a6a1443c
Ethan Collins <ethantcollins98@gmail.com>
2020-01-30 15:14:25 -0700

    adds knex/knex file structure, and adds migration files

---
+++ knex/migrations/20200130145917_create_users.js
@@ -0,0 +1,10 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("users", t => {
+    t.increments("id");
+    t.string("name");
+    t.string("email");
+    t.string("password");
+  });
+};
+
+exports.down = function(knex) {};
